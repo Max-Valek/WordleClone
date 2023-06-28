3760eec4918d8aee933c4bd189d1e6f8787237cd
Max Valek <valek.maxwell@gmail.com>
2023-06-28 10:36:13 -0500

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Max Valek on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
